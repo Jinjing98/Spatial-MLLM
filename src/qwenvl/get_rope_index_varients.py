317ebc6b204b3af73780c975202f2dc14fe8a00f
jinjing98 <xjj892290531@163.com>
2026-02-02 13:16:14 +0100

    obtain visual_token_mask

--- src/qwenvl/get_rope_index_varients.py
+++ src/qwenvl/get_rope_index_varients.py
@@ -79,6 +79,14 @@ def get_rope_index_mRoPE_woT(
             dtype=input_ids.dtype,
             device=input_ids.device,
         )
+        # # JJ: visual patch token mask (B, L)
+        visual_token_mask = torch.zeros(
+            input_ids.shape[0],
+            input_ids.shape[1],
+            dtype=torch.long,
+            device=input_ids.device,
+        )
+
         image_index, video_index = 0, 0
         attention_mask = attention_mask.to(total_input_ids.device)
         for i, input_ids in enumerate(total_input_ids):
@@ -90,6 +98,7 @@ def get_rope_index_mRoPE_woT(
             video_nums = (vision_tokens == video_token_id).sum()
             input_tokens = input_ids.tolist()
             llm_pos_ids_list: list = []
+            llm_visual_mask_list: list = [] #JJ
             st = 0
             remain_images, remain_videos = image_nums, video_nums
             for _ in range(image_nums + video_nums):
@@ -130,13 +139,22 @@ def get_rope_index_mRoPE_woT(
                     h.item() // spatial_merge_size,
                     w.item() // spatial_merge_size,
                 )
+                # -------------------------
+                # Text segment before <image>/<video> token
+                # -------------------------
                 text_len = ed - st
 
                 st_idx = llm_pos_ids_list[-1].max() + 1 if len(llm_pos_ids_list) > 0 else 0
                 llm_pos_ids_list.append(torch.arange(text_len).view(1, -1).expand(3, -1) + st_idx)
 
+                # JJ
+                llm_visual_mask_list.append(torch.zeros(text_len, dtype=torch.long, device=input_ids.device))
+
+                # -------------------------
+                # Vision patch segment
+                # -------------------------
                 range_tensor = torch.arange(llm_grid_t).view(-1, 1)
-                # JJ. The only difference compared to mRoPE
+                # JJ. The only difference compared to mRoPE: set T dim as 0 for all video tokens
                 range_tensor = torch.zeros_like(range_tensor)
 
                 expanded_range = range_tensor.expand(-1, llm_grid_h * llm_grid_w)
@@ -149,19 +167,32 @@ def get_rope_index_mRoPE_woT(
                 h_index = torch.arange(llm_grid_h).view(1, -1, 1).expand(llm_grid_t, -1, llm_grid_w).flatten()
                 w_index = torch.arange(llm_grid_w).view(1, 1, -1).expand(llm_grid_t, llm_grid_h, -1).flatten()
                 llm_pos_ids_list.append(torch.stack([t_index, h_index, w_index]) + text_len + st_idx)
-                st = ed + llm_grid_t * llm_grid_h * llm_grid_w
-
+                
+                # JJ
+                vision_len = llm_grid_t * llm_grid_h * llm_grid_w        
+                llm_visual_mask_list.append(torch.ones(vision_len, dtype=torch.long, device=input_ids.device))
+ 
+                st = ed + vision_len #llm_grid_t * llm_grid_h * llm_grid_w
+            
+            # remaining trailing text
             if st < len(input_tokens):
                 st_idx = llm_pos_ids_list[-1].max() + 1 if len(llm_pos_ids_list) > 0 else 0
                 text_len = len(input_tokens) - st
                 llm_pos_ids_list.append(torch.arange(text_len).view(1, -1).expand(3, -1) + st_idx)
+                llm_visual_mask_list.append(torch.zeros(text_len, dtype=torch.long, device=input_ids.device))# JJ
+
 
             llm_positions = torch.cat(llm_pos_ids_list, dim=1).reshape(3, -1)
+            llm_visual_mask = torch.cat(llm_visual_mask_list, dim=0).reshape(-1) # JJ
+            
             position_ids[..., i, attention_mask[i] == 1] = llm_positions.to(position_ids.device)
+            visual_token_mask[i, attention_mask[i] == 1] = llm_visual_mask # JJ
+
             mrope_position_deltas.append(llm_positions.max() + 1 - len(total_input_ids[i]))
         mrope_position_deltas = torch.tensor(mrope_position_deltas, device=input_ids.device).unsqueeze(1)
-        return position_ids, mrope_position_deltas
+        return position_ids, mrope_position_deltas, visual_token_mask
     else:
+        # No vision case
         if attention_mask is not None:
             position_ids = attention_mask.long().cumsum(-1) - 1
             position_ids.masked_fill_(attention_mask == 0, 1)
@@ -180,4 +211,6 @@ def get_rope_index_mRoPE_woT(
                 dtype=input_ids.dtype,
             )
 
-        return position_ids, mrope_position_deltas
+        # JJ: no-vision => visual_token_mask all zeros
+        visual_token_mask = torch.zeros(input_ids.shape[0], input_ids.shape[1], dtype=torch.long, device=input_ids.device)
+        return position_ids, mrope_position_deltas, visual_token_mask
